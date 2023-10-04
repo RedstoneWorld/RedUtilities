578b15523f6e37b65785bff63cb87316113cec43
RedstoneFuture <redstonefuture@redstoneworld.de>
2023-10-04 03:45:17 +0200

    Adding mathematical methods

--- src/main/java/de/redstoneworld/redutilities/input/InputFormat.java
+++ src/main/java/de/redstoneworld/redutilities/input/InputFormat.java
@@ -4,16 +4,42 @@ public class InputFormat {
 
     /**
      * This method validate the input string and check if it's a
-     * valid natural number without a signum of a permutation, and 
-     * it's also greater than zero.
+     * valid natural number without a signum of a permutation.
      *
      * @return 'true' if the input is a natural number
      */
-    public static boolean isPositiveNaturalNumber(String cmdInput) {
+    public static boolean isNaturalNumber(String cmdInput) {
         if (cmdInput.matches("^[0-9]+$")) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * This method validate the input string and check if it's a
+     * valid natural number without a signum of a permutation, and 
+     * it's also greater than zero.
+     *
+     * @return 'true' if the input is a positive natural number
+     */
+    public static boolean isPositiveNaturalNumber(String cmdInput) {
+        if (isNaturalNumber(cmdInput)) {
             if (Integer.parseInt(cmdInput) > 0) {
                 return true;
             }
+        }
+        return false;
+    }
+
+    /**
+     * This method validate the input string and check if it's a
+     * valid integer.
+     *
+     * @return 'true' if the input is an integer
+     */
+    public static boolean isInteger(String cmdInput) {
+        if (cmdInput.matches("^[-\\+]?[0-9]+$")) {
+            return true;
         }
         return false;
     }
@@ -25,7 +51,6 @@ public class InputFormat {
      * @return 'true' if the input is a rationale number
      */
     public static boolean isRationaleNumber(String cmdInput) {
-
         if (cmdInput.matches("^[-\\+]?[0-9]*[\\.,]?[0-9]+$")) {
             return true;
         }
