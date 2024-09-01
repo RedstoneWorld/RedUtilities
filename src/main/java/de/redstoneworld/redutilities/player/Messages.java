d3db055269fdd6d91e355353b1c81d343bb56bcc
RedstoneFuture <redstonefuture@redstoneworld.de>
2024-09-01 16:05:15 +0200

    Improving description of "sendActionbarMsg" method

--- src/main/java/de/redstoneworld/redutilities/player/Messages.java
+++ src/main/java/de/redstoneworld/redutilities/player/Messages.java
@@ -6,6 +6,13 @@ import org.bukkit.entity.Player;
 
 public class Messages {
 
+    /**
+     * This method sends the desired message to the player as an 
+     * <a href="https://minecraft.wiki/w/Action_bar" target="_blank">Actionbar</a> message.
+     * 
+     * @param player (Player) the target player
+     * @param message (String) the Actionbar message
+     */
     @SuppressWarnings("deprecation")
     public static void sendActionbarMsg(Player player, String message) {
         player.sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
