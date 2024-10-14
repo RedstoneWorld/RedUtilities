090de1b2546d42c5a59679122c4fb40803247819
RedstoneFuture <redstonefuture@redstoneworld.de>
2024-10-14 22:48:32 +0200

    Adding EntityHelper class with corresponding methods

---
+++ src/main/java/de/redstoneworld/redutilities/entity/EntityHelper.java
@@ -0,0 +1,111 @@
+package de.redstoneworld.redutilities.entity;
+
+import io.papermc.paper.tag.EntityTags;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Tag;
+import org.bukkit.entity.EntityType;
+
+import java.lang.reflect.Field;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class EntityHelper {
+
+    /**
+     * This method returns a list of EntityTypes based on the Set of input strings.
+     * 
+     * @param inputSet (Set of Strings) the input String
+     * @return (Set of EntityTypes) the resulted EntityTypes
+     */
+    public static Set<EntityType> getEntityTypes(Set<String> inputSet) {
+        if ((inputSet == null) || (inputSet.isEmpty())) return null;
+        
+        Set<EntityType> entities = EnumSet.noneOf(EntityType.class);
+        
+        for (String input : inputSet.stream().toList()) {
+            entities.addAll(getEntityTypes(input));
+        }
+        
+        return entities;
+    }
+    
+    /**
+     * This method returns a list of EntityTypes based on the input string. Various formats 
+     * of specifications are supported here:
+     * 
+     * <li>EntityType-Tag starting with "#" or "tag=" (see <a href="https://jd.papermc.io/paper/1.21.1/org/bukkit/Tag.html">PaperMC Java-Doc</a> and 
+     * <a href="https://minecraft.wiki/w/Tag#Entity_type_tags_2">Minecraft-Wiki</a> for the EntityType Tag lists)</li>
+     * <li>Regex via "r="</li>
+     * <li>ENTITY-TYPE names with wildcards via "*"</li>
+     * <li>ENTITY-TYPE names</li>
+     * 
+     * Basic-Design by <a href="https://github.com/Phoenix616">Phoenix616</a>
+     * 
+     * @param input (String) the input String
+     * @return (Set of EntityTypes) the resulted EntityTypes
+     */
+    public static Set<EntityType> getEntityTypes(String input) {
+        if ((input == null) || (input.isEmpty())) return null;
+        
+        Set<EntityType> entities = EnumSet.noneOf(EntityType.class);
+        
+        // EntityType-Tag Definition:
+        if ((input.startsWith("#")) || (input.startsWith("tag="))) {
+            String nameSpace = "";
+            String tagName = "";
+            
+            if (input.startsWith("#")) {
+                tagName = input.substring(1).toLowerCase();
+            } else if (input.startsWith("tag=")) {
+                tagName = input.substring(4).toLowerCase();
+            }
+            
+            String[] parts = tagName.split(":");
+            if (parts.length == 1) {
+                nameSpace = NamespacedKey.MINECRAFT;
+                tagName = parts[0].toLowerCase();
+            } else if (parts.length == 2) {
+                nameSpace = parts[0].toLowerCase();
+                tagName = parts[1].toLowerCase();
+            }
+            
+            Tag<EntityType> tag;
+            
+            // EntityTypes:
+            tag = Bukkit.getTag(Tag.REGISTRY_ENTITY_TYPES, new NamespacedKey(nameSpace, tagName), EntityType.class);
+            
+            if (tag == null) {
+                try {
+                    Field field = EntityTags.class.getField(tagName);
+                    tag = (Tag<EntityType>) field.get(null);
+                } catch (NoSuchFieldException | IllegalAccessException e) {
+                    throw new IllegalArgumentException(e.getMessage());
+                }
+            }
+
+            if (tag != null) {
+                entities.addAll(tag.getValues());
+            }
+            
+        // Regex Definition:
+        } else if (input.startsWith("r=") || input.contains("*")) {
+            Pattern p = Pattern.compile(input.startsWith("r=") ? input.substring(2) : input.replace("*", "(.*)"));
+            
+            for (EntityType entity : EntityType.values()) {
+                if (p.matcher(entity.name()).matches()) {
+                    entities.add(entity);
+                }
+            }
+            
+        // clean EntityType-Name Definition:
+        } else {
+            entities.add(EntityType.valueOf(input.toUpperCase()));
+            
+        }
+        
+        return entities;
+    }
+    
+}
