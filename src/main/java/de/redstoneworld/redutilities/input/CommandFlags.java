b5cbda6bef0f5e175cd853f42991525014b92793
RedstoneFuture <redstonefuture@redstoneworld.de>
2025-07-21 17:49:00 +0200

    Adding help methods to parse flag arguments

--- src/main/java/de/redstoneworld/redutilities/input/CommandFlags.java
+++ src/main/java/de/redstoneworld/redutilities/input/CommandFlags.java
@@ -1,5 +1,7 @@
 package de.redstoneworld.redutilities.input;
 
+import java.util.stream.IntStream;
+
 public class CommandFlags {
 
     /**
@@ -12,5 +14,51 @@ public class CommandFlags {
         }
         return false;
     }
+
+    /**
+     * This method checks whether the specified flag was found 
+     * for the target argument.
+     *
+     * The use of wildcards (*) is supported for the end of the 
+     * string. This is useful for flags with arguments 
+     * (Example: '-target:*').
+     * 
+     * @param args The array with the command argument input
+     * @param flag The flag to be checked
+     * @param arg The number (Array index) of the argument to be analysed
+     * @return (Boolean) 'true', if the flag was found here
+     */
+    public static boolean isFlag(String[] args, String flag, int arg) {
+        return isFlag(args, flag, arg, arg);
+    }
     
+    /**
+     * This method checks whether the specified flag was found 
+     * for the target arguments.
+     *
+     * The use of wildcards (*) is supported for the end of the 
+     * string. This is useful for flags with arguments 
+     * (Example: '-target:*').
+     * 
+     * @param args The array with the command argument input
+     * @param flag The flag to be checked
+     * @param minArg The minimal number (Array index) of the arguments to be analysed
+     * @param maxArg The maximal number (Array index) of the arguments to be analysed
+     * @return (Boolean) 'true', if the flag was found here
+     */
+    public static boolean isFlag(String[] args, String flag, int minArg, int maxArg) {
+        return IntStream.rangeClosed(minArg, maxArg)
+                .filter(i -> args.length > i)
+                .mapToObj(i -> args[i])
+                .anyMatch(s -> {
+                    
+                    if (flag.endsWith("*")) {
+                        return s.startsWith(flag.replace("*", ""));
+                    } else {
+                        return s.equalsIgnoreCase(flag);
+                    }
+                
+                });
+    }
+
 }
