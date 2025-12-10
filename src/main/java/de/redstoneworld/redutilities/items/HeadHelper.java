90981b2a6de0f25de5ce6c93976120229ef2a7d1
RedstoneFuture <redstonefuture@redstoneworld.de>
2025-12-10 02:25:23 +0100

    Replacing method to get heads with custom texture (base64) with PaperMC methods

--- src/main/java/de/redstoneworld/redutilities/items/HeadHelper.java
+++ src/main/java/de/redstoneworld/redutilities/items/HeadHelper.java
@@ -1,13 +1,11 @@
 package de.redstoneworld.redutilities.items;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
+import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.util.UUID;
 
 public class HeadHelper {
@@ -20,50 +18,17 @@ public class HeadHelper {
      * @return ItemStack of the skull with the specified texture
      */
     public static ItemStack getCustomHead(String base64Texture) {
-        // Create a new ItemStack of type PLAYER_HEAD
-        ItemStack skull = new ItemStack(Material.PLAYER_HEAD, 1);
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
+        if (skullMeta == null) return head;
 
-        // Get the SkullMeta from the ItemStack
-        SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
-        if (skullMeta == null) {
-            return skull; // Return default skull if meta cannot be modified
-        }
+        PlayerProfile profile = org.bukkit.Bukkit.createProfile(UUID.randomUUID(), null);
 
-        // Create a GameProfile with a random UUID and dummy name
-        GameProfile profile = new GameProfile(UUID.randomUUID(), "dummy_name");
-        profile.getProperties().put("textures", new Property("textures", base64Texture));
+        profile.getProperties().add(new ProfileProperty("textures", base64Texture));
 
-        try {
-            // Access the "profile" field dynamically
-            Field profileField = null;
-            for (Field field : skullMeta.getClass().getDeclaredFields()) {
-                if (field.getType().getName().contains("ResolvableProfile") || field.getType().equals(GameProfile.class)) {
-                    profileField = field;
-                    break;
-                }
-            }
+        skullMeta.setPlayerProfile(profile);
+        head.setItemMeta(skullMeta);
 
-            if (profileField != null) {
-                profileField.setAccessible(true);
-
-                if (profileField.getType().getName().contains("ResolvableProfile")) {
-                    // Create a ResolvableProfile wrapper for the GameProfile
-                    Constructor<?> constructor = profileField.getType().getConstructor(GameProfile.class);
-                    Object resolvableProfile = constructor.newInstance(profile);
-                    profileField.set(skullMeta, resolvableProfile);
-                } else {
-                    // For older versions, set the GameProfile directly
-                    profileField.set(skullMeta, profile);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // Apply the modified SkullMeta to the ItemStack
-        skull.setItemMeta(skullMeta);
-
-        return skull;
+        return head;
     }
-
 }
